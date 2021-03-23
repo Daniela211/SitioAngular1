9e15ab3f285619b31a24b4c39fe515f438e30566
Daniela21 <201821158@tese.edu.mx>
2021-03-23 16:07:25 -0600

    cambios para agregar los superheroes en lista dinamica

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero}from "./hero";
+
+export const HEROES:Hero[]= [
+{id: 11, name: "Superman", edad:30,SuperPoder: "Volar", identidad: "Conocida",universo: "DC"},
+{id: 12, name: "Batman",edad:30,SuperPoder: "Detective", identidad: "Desconocida", universo: "DC"},
+{id: 13, name: "Hulk", edad:40,SuperPoder: "Fuerza", identidad: "Desconocida", universo: "MARVEL"},
+{id: 14, name: "Ironman", edad:25,SuperPoder: "Volar", identidad: "Conocida", universo: "MARVEL"},
+{id: 15, name: "Thor",edad:1000,SuperPoder: "Fuerza", identidad: "Conocida", universo: "MARVEL"},
+{id: 16, name: "Black Widow",edad:28,SuperPoder: "Velocidad", identidad: "Conocida", universo: "MARVEL"},
+{id: 17, name: "Flash", edad:28,SuperPoder: "Velocidad", identidad: "Desconocida", universo: "DC"},
+{id: 18, name: "Bruja Escarlata",edad:36,SuperPoder: "Brujería", identidad: "Conocida", universo: "MARVEL"},
+{id: 19, name: "Spiderman", edad:22,SuperPoder: "Escalar", identidad: "Desconocida", universo: "MARVEL"},
+{id: 11, name: "Wonder woman",edad: 35,SuperPoder: "Volar", identidad: "Conocida", universo: "DC"},
+];
